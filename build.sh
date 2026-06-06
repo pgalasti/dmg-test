5a6eff93b802ddb329e934a069a27953917d217b
Paul Galasti <pgalasti@gmail.com>
2026-06-06 13:55:31 -0400

    Added readme and fixing steps

--- build.sh
+++ build.sh
@@ -7,5 +7,8 @@ rgbasm -Werror -Weverything -o test.o test.asm
 rgblink --dmg --tiny -o test.gb test.o
 
 # Required Post link fix
+# Title must be undeer 11 characters
 rgbfix --title paul-test --pad-value 0 --validate test.gb
 
+echo "ROM Built"
+echo "Maybe quick check context with heapdump -C test.gb?"
--- test.asm
+++ test.asm
@@ -2,8 +2,8 @@ def ROM_HEADER_ADDRESS    equ $0100
 def ROM_MAIN_ADDRESS      equ $0150
 
 section "header", rom0[ROM_HEADER_ADDRESS]
-  di
-  jr main
+  di          ; 1 byte instruction to disable interupts
+  jr main     ; 2 byte instruction to jump to main
 
 section "main", rom0[ROM_MAIN_ADDRESS]
 main:
