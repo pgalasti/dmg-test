673878d2ecc2a353dddad8cba8748a2637d3a358
Paul Galasti <pgalasti@gmail.com>
2026-06-06 13:49:30 -0400

    Added linker and a required 'fix' step

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Assemble
 rgbasm -Werror -Weverything -o test.o test.asm
+
+# Link 
+rgblink --dmg --tiny -o test.gb test.o
+
+# Required Post link fix
+rgbfix --title paul-test --pad-value 0 --validate test.gb
+
